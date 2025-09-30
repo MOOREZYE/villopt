9717fdbb65d4cd0ffc46d7b3072de804fd8c3ea0
MOOREZYE <73567259+MOOREZYE@users.noreply.github.com>
2025-09-30 10:44:41 +0100

    Add files via upload

--- VillOpt/app.py
+++ VillOpt/app.py
@@ -145,4 +145,4 @@ def generate_invoices(filepath):
         c.save()
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000))) 
